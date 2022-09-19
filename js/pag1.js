f11127136c08f222d7d39b36497b2c123024d1a8
the evil <caiofgomesrj@gmail.com>
2022-09-19 16:58:07 -0300

    codigo completo

---
+++ js/pag1.js
@@ -0,0 +1,8 @@
+function mostrar(){
+    var a = document.getElementById('pontos2').style.visibility = 'visible';
+    
+}function chu(){
+    window.location.href = "pg1chu.html";
+    window.open ="";
+
+}
